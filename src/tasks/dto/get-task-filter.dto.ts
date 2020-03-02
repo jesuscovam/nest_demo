9e5382f4b2b5ded0f9dcac2b940ea8b59553b14f
jesuscova <jesuscovam@icloud.com>
2020-03-02 18:57:48 -0500

    added isOptional validator and IsIn for status

--- src/tasks/dto/get-task-filter.dto.ts
+++ src/tasks/dto/get-task-filter.dto.ts
@@ -1,6 +1,11 @@
 import { TaskStatus } from "../task.module";
+import { IsOptional, IsIn } from "class-validator";
 
 export class GetTaskFilterDto {
+    @IsOptional()
+    @IsIn([...Object.values(TaskStatus)])
     status: TaskStatus;
+
+    @IsOptional()
     search: string;
 }
