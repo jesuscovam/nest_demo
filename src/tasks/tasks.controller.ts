e34efb859ea6c81822f2546bbcd3a8f33cf96bb0
jesuscova <jesuscovam@icloud.com>
2020-03-02 19:00:56 -0500

    added validationType to Query in getTasks controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -5,12 +5,13 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { TaskValidationPipe } from './pipes/task-validation-pipe';
 
+
 @Controller('tasks')
 export class TasksController {
     constructor(private taskServices: TasksService){}
 
     @Get()
-    getTasks(@Query() filterDto: GetTaskFilterDto): Task[] {
+    getTasks(@Query(ValidationPipe)  filterDto: GetTaskFilterDto ): Task[] {
         if(Object.keys(filterDto).length) {
             return this.taskServices.getTaskWithFilters(filterDto);
         } else {
